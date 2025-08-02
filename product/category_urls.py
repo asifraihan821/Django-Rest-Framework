de9808f7078f166a7457be429859c21e52446727
asibur rahman <asifraihan821@gmail.com>
2025-08-02 16:50:00 +0600

    get_object_or_404 function understanding

--- product/category_urls.py
+++ product/category_urls.py
@@ -4,6 +4,6 @@ from product import views
 
 
 urlpatterns = [
-    path('',views.view_category,name='categories'),
+    path('<int:id>/',views.view_category,name='categories'),
     path('nothing/',views.view_nothing),
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -8,20 +8,20 @@ from product.models import Category,Product
 
 @api_view()
 def view_product(request,id):
-    try:
-        product = Product.objects.get(pk=id)
-        product_dict = {
-            'id':product.id,'name':product.name,'price':product.price
-        }
-        return Response(product_dict)
-    except Product.DoesNotExist:
-        return Response({'message': 'Product Does not exist'},status=404)
+    product = get_object_or_404(Product,pk=id)
+    product_dict = {
+        'id':product.id,'name':product.name,'price':product.price
+    }
+    return Response(product_dict)
 
 
 
 @api_view()
-def view_category(request):
-    return Response({'message':'Categories'})
+def view_category(request,id):
+    cat = get_object_or_404(Category,pk=id)  # get_object_of_404 takes the model name and pk argument as argument
+    cat_dict = {'name':cat.name,'des':cat.description}
+    return Response(cat_dict)
+
 
 @api_view()
 def view_nothing(request):

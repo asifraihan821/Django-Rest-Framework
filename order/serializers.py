90d985c7694b5eede9a5cb78d7c191f9e49f500e
asibur rahman <asifraihan821@gmail.com>
2025-08-06 17:02:17 +0600

    Delete cArt & GetCart Items

--- order/serializers.py
+++ order/serializers.py
@@ -21,7 +21,7 @@ class CartItemSerializer(serializers.ModelSerializer):
         return cart_item.quantity * cart_item.product.price
 
 class CartSerializer(serializers.ModelSerializer):
-    items = CartItemSerializer(many=True)
+    items = CartItemSerializer(many=True, read_only=True)
     total_price = serializers.SerializerMethodField(method_name='get_total_price')
 
     class Meta:

e4562187a7c53d33c2e87a3120d4b9b5836082b1
asibur rahman <asifraihan821@gmail.com>
2025-08-06 11:06:00 +0600

    Creating & Getting Cart

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path,include
 from rest_framework_nested import routers
 from product.views import ProductViewSet,CategoryViewSet,ReviewViewSet
+from order.views import CartViewSet
+
+
+
 
 router = routers.DefaultRouter()
 router.register('products',ProductViewSet, basename='product')
 router.register('categories',CategoryViewSet)
+router.register('carts',CartViewSet, basename='carts')
 
 product_router = routers.NestedDefaultRouter(router,'products',lookup='product')
 

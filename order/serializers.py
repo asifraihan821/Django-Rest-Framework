a3591eba7e1fcc28db4c7bc00e56cbd0501b4b2c
asibur rahman <asifraihan821@gmail.com>
2025-08-14 14:56:37 +0600

    24.7 porjonto done

--- order/serializers.py
+++ order/serializers.py
@@ -82,25 +82,24 @@ class CartSerializer(serializers.ModelSerializer):
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
-    def validate_cart_id(self, cart_id):
+    def validate_cart_id(self,cart_id):
         if not Cart.objects.filter(pk=cart_id).exists():
             raise serializers.ValidationError('No cart found with this id')
-
+        
         if not CartItem.objects.filter(cart_id=cart_id).exists():
-            raise serializers.ValidationError('Cart is empty')
-
+            raise serializers.ValidationError('Cart is Empty')
+        
         return cart_id
-
+    
     def create(self, validated_data):
         user_id = self.context['user_id']
         cart_id = validated_data['cart_id']
 
         try:
             order = OrderService.create_order(user_id=user_id, cart_id=cart_id)
-            return order
         except ValueError as e:
             raise serializers.ValidationError(str(e))
-
+    
     def to_representation(self, instance):
         return OrderSerializer(instance).data
 
@@ -118,21 +117,21 @@ class UpdateOrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['status']
 
-    def update(self,instance, validated_data):
-        user = self.context['user']
+
+    def update(self,instance,validated_data):
+        user= self.context['user']
         new_status = validated_data['status']
 
         if new_status == Order.CANCELED:
             return OrderService.cancel_order(order=instance,user=user)
-        if not user.is_staff:
-            raise serializers.ValidationError('You are not allowed to update this order')
         
-        return super().update(instance, validated_data)
-
+        if not user.is_staff:
+            raise serializers.ValidationError('detail: You are not allowe to delete this order')
+        
+        return super().update(instance,validated_data)
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
-
     class Meta:
         model = Order
         fields = ['id', 'user', 'status', 'total_price', 'created_at', 'items']
--- order/views.py
+++ order/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from order import serializers as orderSz
-from order.serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
+from order.serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer,OrderSerializer, CreateOrderSerializer,UpdateOrderSerializer
 from order.models import Cart, CartItem, Order, OrderItem
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.decorators import action
@@ -18,8 +18,11 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gener
     serializer_class = CartSerializer
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     def get_queryset(self):
-        return Cart.objects.filter(user=self.request.user)
+        return Cart.objects.prefetch_related('items__product').filter(user=self.request.user)
 
 
 class CartItemViewSet(ModelViewSet):
@@ -40,39 +43,23 @@ class CartItemViewSet(ModelViewSet):
 
 
 class OrderViewset(ModelViewSet):
-    http_method_names = ['get', 'post', 'delete', 'patch', 'head', 'options']
-
-    @action(detail=True, methods=['post'])
-    def cancel(self, request, pk=None):
-        order = self.get_object()
-        OrderService.cancel_order(order=order, user=request.user)
-        return Response({'status': 'Order canceled'})
-
-    @action(detail=True, methods=['patch'])
-    def update_status(self, request, pk=None):
-        order = self.get_object()
-        serializer = orderSz.UpdateOrderSerializer(
-            order, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'status': f'Order status updated to {request.data['status']}'})
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['get','post','patch','head','options']
 
     def get_permissions(self):
-        if self.action in ['update_status', 'destroy']:
+        if self.request.method == 'DELETE':
             return [IsAdminUser()]
         return [IsAuthenticated()]
 
     def get_serializer_class(self):
-        if self.action == 'cancel':
-            return orderSz.EmptySerializer
-        if self.action == 'create':
-            return orderSz.CreateOrderSerializer
-        elif self.action == 'update_status':
-            return orderSz.UpdateOrderSerializer
-        return orderSz.OrderSerializer
-
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateOrderSerializer
+        return OrderSerializer
+    
     def get_serializer_context(self):
-        return {'user_id': self.request.user.id, 'user': self.request.user}
+        return {'user_id': self.request.user.id, 'user':self.request.user}
 
     def get_queryset(self):
         if self.request.user.is_staff:
